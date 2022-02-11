b74af5f6bedd208bfc1cc8dbf6b6adc9c40ba6cd
Parker Higgins <parkershiggins@gmail.com>
2022-02-11 18:34:57 -0500

    Profile page updated with structure mostly correct. Still have some styling work to do.

--- src/components/ProfileSection/ProfileElements.js
+++ src/components/ProfileSection/ProfileElements.js
@@ -2,26 +2,57 @@ import styled from 'styled-components';
 
 export const ProfileContainer = styled.div`
   
-
   display: flex;
-  align-items: center;
+  //align-items: center;
   height: 1050px;
   background: #232323;
+  //margin: 80px auto 0 auto;
+  //max-width: 1200px;
 
  
+`;
+
+export const ProfileContentContainer = styled.div`
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  position: relative;
+  //align-items: center;
+  margin: auto;
+`;
+
+export const ProfilePicContainer = styled.div`
+
+  display: flex;
+  flex-direction: column;
+  //position: relative;
+  //align-items: center;
+  //height: 900px;
+  
+  width: 25%;
+`;
+
+export const ProfilePic = styled.img`
+  
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  justify-content: flex-start;
+  height: 20vh;
+  
 `;
 
 export const ProfileContent = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  //justify-content: center;
   position: relative;
   align-items: center;
 
   height: 900px;
-
-
-
+  width: 50%;
+  
 
 `
 export const UserName = styled.div`
@@ -30,7 +61,7 @@ export const UserName = styled.div`
   justify-content: center;
 
 
-  width: 100vw;
+  width: 40vw;
   color: white;
   font-size: 5rem;
   font-weight: bold;
@@ -39,19 +70,14 @@ export const UserName = styled.div`
 
 export const ProfileText = styled.div`
   margin-top: 5vw;
-
   line-height: 170%;
   font-size: 2.5rem;
   color: white;
-  width: 80vw;
+  width: 20vw;
   max-width: 1200px;
 
   
 `
-
-export const ProfilePic = styled.img`
-  height: 20vh;
-`;
 
 export const FollowButton = styled.div`
 
@@ -60,6 +86,18 @@ export const FollowButton = styled.div`
 export const ProfileStats = styled.div`
 
 `;
+
+export const UserStats = styled.div`
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin: 10px auto 10px auto;
+
+    
+` ;
+
 export const PostsStat = styled.div`
 
 `;
@@ -87,5 +125,7 @@ export const Comments = styled.div`
 export const Saved = styled.div`
 
 `;
+
+
 
 
--- src/components/ProfileSection/index.js
+++ src/components/ProfileSection/index.js
@@ -1,13 +1,142 @@
-import React from 'react';
-
+import * as React from 'react';
+import {AppBar, Box, Button, Card, CardActions, CardContent, Container, useTheme} from "@mui/material";
+import Grid from '@mui/material/Grid';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import ThumbUpAltRoundedIcon from '@mui/icons-material/ThumbUpAltRounded';
+import TextsmsRoundedIcon from '@mui/icons-material/TextsmsRounded';
+import BookmarkRoundedIcon from '@mui/icons-material/BookmarkRounded';
+import PropTypes from 'prop-types';
 
 import {
-    ProfileContainer,
-    ProfileContent, ProfileText, UserName, ProfilePic, FollowButton, ProfileStats, PostsStat, FollowersStat, FollowingStat ,PostContainer, Tabs, Posts, Liked, Comments, Saved
-
-
+    ProfilePic, UserStats,
 } from './ProfileElements';
+
 import pic from "../../images/cat_pic.jpg";
+
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`full-width-tabpanel-${index}`}
+            aria-labelledby={`full-width-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `full-width-tab-${index}`,
+        'aria-controls': `full-width-tabpanel-${index}`,
+    };
+}
+
+function FullWidthTabs() {
+    const theme = useTheme();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    const handleChangeIndex = (index) => {
+        setValue(index);
+    };
+
+    return (
+
+        <Box sx={{ bgcolor: 'orange'}}>
+            <AppBar position="static">
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    indicatorColor="secondary"
+                    textColor="inherit"
+                    variant="fullWidth"
+                    aria-label="full width tabs example"
+                >
+                    <Tab label="Posts" {...a11yProps(0)} />
+                    <Tab label="Liked" {...a11yProps(1)} />
+                    <Tab label="Saved" {...a11yProps(2)} />
+                    <Tab label="Comments" {...a11yProps(3)} />
+                </Tabs>
+            </AppBar>
+                <TabPanel value={value} index={0} dir={theme.direction}>
+                    <OutlinedCard></OutlinedCard>
+                    <OutlinedCard></OutlinedCard>
+                </TabPanel>
+                <TabPanel value={value} index={1} dir={theme.direction}>
+                    <OutlinedCard></OutlinedCard>
+                </TabPanel>
+                <TabPanel value={value} index={2} dir={theme.direction}>
+                    <OutlinedCard></OutlinedCard>
+                </TabPanel>
+                <TabPanel value={value} index={3} dir={theme.direction}>
+                    <OutlinedCard></OutlinedCard>
+                </TabPanel>
+        </Box>
+    );
+}
+
+const card = (
+    <React.Fragment>
+        <CardContent>
+            {/*<Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>*/}
+            {/*    Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.*/}
+            {/*</Typography>*/}
+            <Typography variant="h6" component="div">
+                Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Typography>
+        </CardContent>
+        <CardActions>
+            <Container>
+                <Grid
+                    container
+                    direction="row"
+                    justifyContent="flex-start"
+                    alignItems="center">
+                    <ThumbUpAltRoundedIcon></ThumbUpAltRoundedIcon>
+                    <TextsmsRoundedIcon></TextsmsRoundedIcon>
+                </Grid>
+            </Container>
+            <Container>
+                <Grid
+                    container
+                    direction="row"
+                    justifyContent="flex-end"
+                    alignItems="center">
+                    <BookmarkRoundedIcon></BookmarkRoundedIcon>
+                </Grid>
+            </Container>
+        </CardActions>
+    </React.Fragment>
+);
+
+function OutlinedCard() {
+    return (
+        <Box sx={{ minWidth: 275 }}>
+            <Card variant="outlined">{card}</Card>
+        </Box>
+    );
+}
 
 
 function ProfileSection() {
@@ -15,40 +144,161 @@ function ProfileSection() {
 
     return (
 
-        <ProfileContainer id='aboutMe'>
-            <ProfileContent>
-                <ProfilePic src={pic}/>
-                <UserName>Cat Dude</UserName>
+        <Container fixed>
+            <Grid
+                container
+                direction="row"
+                justifyContent="center"
+                alignItems="flex-start"
+                spacing={2}
+            >
 
-                <FollowButton>Follow</FollowButton>
+                <Grid
+                    // LHS Column
+                    container
+                    direction="column"
+                    justifyContent="flex-start"
+                    alignItems="center"
+                    item xs={4}
+                >
+                    <ProfilePic src={pic}/>
 
-                <ProfileStats>
-                    <PostsStat>Posts: 87</PostsStat>
-                    <FollowersStat>90</FollowersStat>
-                    <FollowingStat>10</FollowingStat>
-                </ProfileStats>
+                </Grid>
 
-                <PostContainer>
-                    <Tabs>
-                        <Posts></Posts>
-                        <Liked></Liked>
-                        <Comments></Comments>
-                        <Saved></Saved>
-                    </Tabs>
+                <Grid
+                    // RHS Column
+                    container
+                    direction="column"
+                    justifyContent="flex-start"
+                    alignItems="stretch"
+                    item xs={8}
+                >
 
-                </PostContainer>
+                    <Grid
+                        // Name and Follow Button
+                        container
+                        direction="column"
+                        justifyContent="flex-start"
+                        alignItems="flex-start"
+                    >
+                        <h1>Cat Dude</h1>
 
-                <ProfileText> stuff
+                        <Grid
+                            // Follow Button container
+                            container
+                            direction="column"
+                            justifyContent="flex-start"
+                            alignItems="flex-start"
+                        >
 
-                </ProfileText>
+                            <Button
+                                container
+                                direction="column"
+                                justifyContent="center"
+                                alignItems="center"
+                                // fullWidth={true}
 
-            </ProfileContent>
+                                variant="outlined">Follow</Button>
+
+                        </Grid>
+
+                    </Grid>
 
 
+                    {/*<UserStats>*/}
+                    <Grid
+                        // User Stats
+                        container
+                        direction="row"
+                        alignItems="center"
+                        justifyContent="center"
+                        spacing={2}
+                    >
+                        <Grid
+                            container
+                            direction="column"
+                            alignItems="center"
+                            justifyContent="center"
+                            item xs={4}
+                        >
+                            <p>Posts:</p>
+                            <Button variant="text">####</Button>
+                        </Grid>
+                        <Grid container
+                              direction="column"
+                              alignItems="center"
+                              justifyContent="center"
+                              item xs={4}
+                        >
+                            <p>Followers:</p>
+                            <Button variant="text">####</Button>
+                        </Grid>
+                        <Grid container
+                              direction="column"
+                              alignItems="center"
+                              justifyContent="center"
+                              item xs={4}
+                        >
+                            <p>Following:</p>
+                            <Button variant="text">####</Button>
+                        </Grid>
+                    </Grid>
+                    {/*</UserStats>*/}
 
 
+                    <FullWidthTabs></FullWidthTabs>
 
-        </ProfileContainer>
+                </Grid>
+            </Grid>
+        </Container>
+
+
+        // <ProfileContainer> {/*Page*/}
+        //     <ProfileContentContainer>
+        //         {/*Content Wrapper*/}
+        //
+        //         {/*<ProfilePic src={pic}/>*/}
+        //         <ProfilePicContainer>
+        //
+        //             <ProfilePic src={pic}/>
+        //         </ProfilePicContainer>
+        //
+        //         <ProfileContent>
+        //
+        //             <UserName>Cat Dude</UserName>
+        //
+        //             <FollowButton>Follow</FollowButton>
+        //
+        //             <ProfileStats>
+        //                 <PostsStat>Posts: 87</PostsStat>
+        //                 <FollowersStat>90</FollowersStat>
+        //                 <FollowingStat>10</FollowingStat>
+        //             </ProfileStats>
+        //
+        //             <PostContainer>
+        //                 <Tabs>
+        //                     <Posts></Posts>
+        //                     <Liked></Liked>
+        //                     <Comments></Comments>
+        //                     <Saved></Saved>
+        //                 </Tabs>
+        //
+        //             </PostContainer>
+        //
+        //             <ProfileText>
+        //                 stuff
+        //
+        //             </ProfileText>
+        //
+        //         </ProfileContent>
+        //
+        //    </ProfileContentContainer>
+        //
+        //
+        //
+        //
+        //
+        // </ProfileContainer>
     );
 }
 
