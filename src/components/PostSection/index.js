5c2a68df818b67f309986416ccbeab2ef4f688c1
rebecca17930329 <2819485382@qq.com>
2022-02-11 16:15:40 -0500

    updated make post button, now it can show icon inside button

--- src/components/PostSection/index.js
+++ src/components/PostSection/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import CreatePostIcon from '@mui/icons-material/Create';
-import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import { Container, Button, Link } from 'react-floating-action-button'
 
 function Post(){
 
     return (
         <Container styles={{color: 'darkblue'}}>
-            <CreatePostIcon/> Make a Post Here <ArrowDownwardIcon/>
+             Make a Post Here
             <Button
                 tooltip="Click to make a new post"
-                icon="fa-plus"
-                rotate={true}
-                styles={{backgroundColor: 'darkblue' }}
+                styles={{backgroundColor: 'darkblue' , color : 'yellow'}}
                 onClick={() => alert('Add post here') }
-            />
+            >
+                <CreatePostIcon/>
+            </Button>
         </Container>
     );
 }
