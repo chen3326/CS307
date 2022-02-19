a37064eee7b4944e7221facf7744fcd2c73142a6
Caroline Lee <caroleen16lee@gmail.com>
2022-02-19 05:20:58 -0500

    firebase tweeks

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,7 @@ import {getFirestore} from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+const app = firebase.initializeApp({
     apiKey: "AIzaSyBNSzngOJk4iyDWUmSRSIF1Qx9TKzB0i10",
     authDomain: "cs307-bdbca.firebaseapp.com",
     projectId: "cs307-bdbca",
@@ -13,8 +13,9 @@ const firebaseConfig = {
     databaseURL: "gs://cs307-bdbca.appspot.com",
     messagingSenderId: "162185983080",
     appId: "1:162185983080:web:af175f3e595466abb937dd"
-};
+})
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
 export const database = getFirestore(app);
+export const auth = app.auth()
+export default app
