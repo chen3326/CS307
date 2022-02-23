9813618d1dfa25a3a722658be157def12411ccf8
rathi4 <rathi4@purdue.edu>
2022-02-23 14:51:57 -0500

    Separated Page for forgot Password and setup firebase functionality for changing logged-in user password Fixed Bleed Error in Navbar

--- src/components/NavbarSection/index.js
+++ src/components/NavbarSection/index.js
@@ -51,7 +51,7 @@ function Navbar(){
 
                         </NavBtn>
 
-                        <NavBtn style={{marginRight: '-300px'}}>
+                        <NavBtn style={{marginRight: '-200px'}}>
                             <NavBtnLink href="\"> Log out </NavBtnLink>
                         </NavBtn>
                     </NavbarContainer>
