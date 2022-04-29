19029f4cbb8524114f72903053981d8ba743ef48
xinchen <cxsdsb@icloud.com>
2022-04-28 20:36:02 -0400

    partly fixed dark mode for side bar pages

--- src/components/PostDisplaySection/saved_post.js
+++ src/components/PostDisplaySection/saved_post.js
@@ -110,16 +110,21 @@ function SavedPost_display() {
                                     <OnePost
                                         postid={post?.id}
                                         title={post?.title}
+                                        location = {post?.location}
                                         topic={post?.topic}
                                         topicAuthor={post?.topicAuthor?.email}
                                         postText={post?.postText}
                                         authorEmail={post?.author?.email}
+                                        authorNickName={post?.author?.display?.nickName}
+                                        authorProfilePic={post?.author?.display?.profilePic}
                                         imageUrl={post?.imageUrl}
                                         imageUrl2={post?.imageUrl2}
                                         imageUrl3={post?.imageUrl3}
                                         FileURl={post?.FileURl}
                                         timestamp={post?.timestamp}
                                         likes={post?.likes}
+                                        authorid={post?.author?.id}
+                                        allowComments={post?.allowComments}
                                     />
                                 ) : (
                                     <div/>
